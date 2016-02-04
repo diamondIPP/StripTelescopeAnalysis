63040fd6292b85375c90bac6556f08ca0375d4ae
Felix Bachmair <bachmair@phys.ethz.ch>
2016-02-04 20:45:35 +0000

    fixed verbosity
    git-svn-id: svn+ssh://utka42.ethz.ch/rd42/branches/3dAnalysis@1569 5e05d84c-b09a-4460-8136-89425579e6ea

--- src/TCluster.cpp
+++ src/TCluster.cpp
@@ -1351,7 +1351,7 @@ Float_t TCluster::getPositiveCharge(UInt_t nClusterEntries, bool cmnCorrected,
  * @return
  */
 bool TCluster::hasNegativeCharge(Float_t& charge, Int_t& pos, bool cmnCorrected) {
-    bool verb = true;
+    bool verb = false;
     Float_t oldCharge = 0;
     Float_t currentCharge;
     if (verb) cout<<"negCharge: "<<endl;
