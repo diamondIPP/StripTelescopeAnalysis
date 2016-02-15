30e5a8c50af5328d27fb5c174a462575f7b3fc47
Felix Bachmair <bachmair@phys.ethz.ch>
2016-02-15 14:18:24 +0000

    bugfixing
    git-svn-id: svn+ssh://utka42.ethz.ch/rd42/branches/3dAnalysis@1630 5e05d84c-b09a-4460-8136-89425579e6ea

--- src/TAnalysisOfAnalysisDifferences.cpp
+++ src/TAnalysisOfAnalysisDifferences.cpp
@@ -226,9 +226,9 @@ void TAnalysisOfAnalysisDifferences::AnalyseSameEvent() {
             Float_t yPredDet = predictedPositions->at(eventNo).second;
             pair<Float_t,Float_t> relPos =  settings->getRelativePositionInCell(xPredDet,yPredDet);
             cout<<"hNoNegativeCharge_RelPosition FILL "<<relPos.first<<"/"<<relPos.second<<endl;
-            cout<< ((TProfile2D*)(mapHistos["hNoNegativeCharge_RelPosition"]))->GetEntries();
-            ((TProfile2D*)(mapHistos["hNoNegativeCharge_RelPosition"]))->Fill(relPos.first,relPos.second);
-            cout<<"-->"<<((TProfile2D*)(mapHistos["hNoNegativeCharge_RelPosition"]))->GetEntries()<<endl;;
+            cout<< ((TH2F*)(mapHistos["hNoNegativeCharge_RelPosition"]))->GetEntries();
+            ((TH2F*)(mapHistos["hNoNegativeCharge_RelPosition"]))->Fill(relPos.first,relPos.second);
+            cout<<"-->"<<((TH2F*)(mapHistos["hNoNegativeCharge_RelPosition"]))->GetEntries()<<endl;;
             if (posCharge < settings->getLowResponseThreshold()){
                 mapHistos["hNoNegativeChargeLowResponsePosition"]->Fill(xPredDet,yPredDet);
             }
