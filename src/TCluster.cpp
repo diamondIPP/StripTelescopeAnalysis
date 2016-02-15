890f23cb7352ad621272214b82386357cf972b44
Felix Bachmair <bachmair@phys.ethz.ch>
2016-02-15 11:30:05 +0000

    hasNegativeCharge doesn't work for clustersize 1
    git-svn-id: svn+ssh://utka42.ethz.ch/rd42/branches/3dAnalysis@1599 5e05d84c-b09a-4460-8136-89425579e6ea

--- src/TCluster.cpp
+++ src/TCluster.cpp
@@ -1382,7 +1382,7 @@ bool TCluster::hasNegativeCharge(Float_t& charge, Int_t& pos, bool cmnCorrected,
         Float_t signal = this->getSignal(clPos,cmnCorrected);
 
         Int_t clusPos = clStart + dir *dif;
-        if (charge != signal)
+        if (charge != signal){
             cout<<"\n"<<clusterSize<<"/"<<clPos<<": Something is wrong: "<<charge<<"/"<<signal<<endl;
             cout<<"clStart: "<<clStart<<" dir: "<<dir<<" dif: "<<dif<<" clusPos:"<<clusPos<<"/"<<clPos<<endl;
             return hasNegativeCharge(charge,pos,cmnCorrected,true);
