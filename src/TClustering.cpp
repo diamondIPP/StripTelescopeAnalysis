714ce9075c7ddd16393c230242439f37b99ffe6e
Diego Alejandro Sanz Becerra <sandiego@phys.ethz.ch>
2016-08-10 00:34:29 +0200

    bla

--- src/TClustering.cpp
+++ src/TClustering.cpp
@@ -152,7 +152,7 @@ void TClustering::clusterEvent()
 }
 
 void TClustering::FillSignalChannelsPlane(UInt_t det, TPlane &plane){
-	for (int ch = 0; ch < TPlaneProperties::getNChannels(plane); ch++){
+	for (int ch = 0; ch < TPlaneProperties::getNChannels(det); ch++){
 		plane.FillPlaneSignalValues(eventReader->getAdcValue(det, ch), eventReader->getPedestalMean(det, ch, false),
 									eventReader->getPedestalMean(det, ch, true), eventReader->getPedestalSigma(det, ch, false),
 									eventReader->getRawSignal(det, ch, false), eventReader->getRawSignal(det, ch, true),
