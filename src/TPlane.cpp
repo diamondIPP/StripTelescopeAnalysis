f0811008f37cf646f8e7234edee4b57fc0192921
Diego Alejandro Sanz Becerra <sandiego@phys.ethz.ch>
2016-08-11 03:58:07 +0200

    changed the constrocturs of TPlane; fixed typo

--- src/TPlane.cpp
+++ src/TPlane.cpp
@@ -31,12 +31,19 @@ TPlane::TPlane(UInt_t planeNo,vector<TCluster> xClusters,TPlaneProperties::enumD
 	this->yClusters.clear();
 	this->type=type;
 	this->planeNo=planeNo;
+	this->adc.clear();
+	this->ped.clear();
+	this->pedCMN.clear();
+	this->pedSigma.clear();
+	this->pedSigmaCMN.clear();
+	this->rawSigna.clear();
+	this->rawSignalCMN.clear();
 }
 
-TPlane::TPlane(UInt_t planeNo,vector<TCluster> xClusters, vector<TCluster> yClusters, TPlaneProperties::enumDetectorType type, Float_t eadc[], Float_t eped[], Float_t epedCMN[], Float_t epedSigma[], Float_t epedSigmaCMN[], Float_t erawSignal[], Float_t erawSignalCMN[], Float_t ecmNoise){
+TPlane::TPlane(UInt_t planeNo,vector<TCluster> xClusters, TPlaneProperties::enumDetectorType type, Float_t eadc[], Float_t eped[], Float_t epedCMN[], Float_t epedSigma[], Float_t epedSigmaCMN[], Float_t erawSignal[], Float_t erawSignalCMN[], Float_t ecmNoise){
 	this->verbosity=0;
 	if(verbosity)cout<<"TPlane:"<<planeNo<<" xClusters:"<<xClusters.size()<<endl;
-	this->SetClusters(xClusters, yClusters);
+	this->SetXClusters(xClusters);
 	this->yClusters.clear();
 	this->type=type;
 	this->planeNo=planeNo;
