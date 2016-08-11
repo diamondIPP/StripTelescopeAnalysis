f9019da673d247554a6cd45902e0f0d7d476a217
Diego Alejandro Sanz Becerra <sandiego@phys.ethz.ch>
2016-08-11 22:30:42 +0200

    bla

--- src/TPlane.cpp
+++ src/TPlane.cpp
@@ -57,15 +57,15 @@ TPlane::TPlane(UInt_t planeNo,vector<TCluster> xClusters,TPlaneProperties::enumD
 //	this->cmNoise = ecmNoise;
 //}
 
-//TPlane::TPlane(UInt_t planeNo,vector<TCluster> xCluster, TPlaneProperties::enumDetectorType type, Float_t *eadc, Float_t *eped, Float_t *epedCMN, Float_t *epedSigma, Float_t *epedSigmaCMN, Float_t *erawSignal, Float_t *erawSignalCMN, Float_t ecmNoise){
-//	this->verbosity=0;
-//	if(verbosity)cout<<"TPlane:"<<planeNo<<" xClusters:"<<xClusters.size()<<endl;
-//	this->SetXClusters(xClusters);
-//	this->yClusters.clear();
-//	this->type=type;
-//	this->planeNo=planeNo;
-//	this->SetSignalValues(eadc, eped, epedCMN, epedSigma, epedSigmaCMN, erawSignal, erawSignalCMN, ecmNoise);
-//}
+TPlane::TPlane(UInt_t planeNo,vector<TCluster> xCluster, TPlaneProperties::enumDetectorType type, Float_t *eadc, Float_t *eped, Float_t *epedCMN, Float_t *epedSigma, Float_t *epedSigmaCMN, Float_t *erawSignal, Float_t *erawSignalCMN, Float_t ecmNoise){
+	this->verbosity=0;
+	if(verbosity)cout<<"TPlane:"<<planeNo<<" xClusters:"<<xClusters.size()<<endl;
+	this->SetXClusters(xClusters);
+	this->yClusters.clear();
+	this->type=type;
+	this->planeNo=planeNo;
+	this->SetSignalValues(eadc, eped, epedCMN, epedSigma, epedSigmaCMN, erawSignal, erawSignalCMN, ecmNoise);
+}
 
 //TPlane::TPlane(UInt_t planeNo,vector<TCluster> xClusters,TPlaneProperties::enumDetectorType type)
 
